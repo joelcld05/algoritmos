1b85788a21c0465555c91b8d11b7aa775c1fbafa
joelcld05 <44384267+joelcld05@users.noreply.github.com>
2020-03-04 16:25:55 -0500

    done

--- app/views.py
+++ app/views.py
@@ -20,7 +20,7 @@ def current_datetime(request):
     conn = pypyodbc.connect(connStr)
     cur = conn.cursor()
     cur.execute("SELECT CreatureID, Name_EN, Name_JP FROM Creatures")
-    rs =[]
+    rs = []
 
     while True:
         row = cur.fetchone()
@@ -54,36 +54,50 @@ def comparewithcsv(name):
                             rsjaro      = jaro.jaro_winkler_metric(name,akaname[1])*100
                             presaundex  = soundex.phonetics(akaname[1])
                             rssound     = jaro.jaro_winkler_metric(originalsoundx,presaundex)*100
-                            rsjarowords = comparationjaro(name, akaname)
-                            if rsjaro >= 85 or rssound >= 90:
+                            rsjarowords = comparationbyword(name, akaname)
+                            if rsjaro >= 90 or rssound >= 90:
                                 results.append([row[0],row[1],akaname,row[3],rsjaro, rssound])
-                            elif rsjarowords[0] >= 85:
-                                results.append({'id':row[0],'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjarowords[0], 'sound':rsjarowords[1]})
-                            elif rsjarowords[1] >= 85 and rsjarowords[0] >= 50:
-                                results.append({'id':row[0],'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjarowords[0], 'sound':rsjarowords[1]})
-
+                            elif rsjarowords[0] >= 90:
+                                results.append({'id':row[0],'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':float("{0:.2f}".format(rsjarowords[0])), 'sound':float("{0:.2f}".format(rsjarowords[1]))})
+                            elif rsjarowords[1] >= 90 and rsjarowords[0] >= 75:
+                                results.append({'id':row[0],'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':float("{0:.2f}".format(rsjarowords[0])), 'sound':float("{0:.2f}".format(rsjarowords[1]))})
+                
                 namecsv     = row[1].upper()
                 rsjaro      = jaro.jaro_winkler_metric(name,namecsv)*100
-                rsjarowords = comparationjaro(name, namecsv)
+                rsjarowords = comparationbyword(name, namecsv)
                 presaundex  = soundex.phonetics(namecsv)
                 rssound     = jaro.jaro_winkler_metric(originalsoundx,presaundex)*100
                 
-                if rsjaro >= 85 :
+                if rsjaro >= 90 :
                     results.append({'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':rsjaro, 'sound':rssound})
-                elif rsjarowords[0] >= 85:
-                    results.append({'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':rsjarowords[0], 'sound':rsjarowords[1]})
+                elif rsjarowords[0] >= 90:
+                    results.append({'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':float("{0:.2f}".format(rsjarowords[0])), 'sound':float("{0:.2f}".format(rsjarowords[1]))})
+                elif rsjarowords[1] >= 90 and rsjarowords[0] >= 75:
+                    results.append({'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':float("{0:.2f}".format(rsjarowords[0])), 'sound':float("{0:.2f}".format(rsjarowords[1]))})
             except:
                 pass
-
-
     results = sorted(results, key=lambda x: x['jaro'], reverse=True)
     return results
 
+def maincomparation(name, compara, soundexoriginal):
+    try:
+        rsjaro      = jaro.jaro_winkler_metric(name,compara)*100
+        rssound     = jaro.jaro_winkler_metric(soundexoriginal,soundex.phonetics(compara))*100
+        rsjarowords = comparationbyword(name, compara)
+        
+        if rsjaro >= 90 :
+            return {'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':rsjaro, 'sound':rssound}
+        elif rsjarowords[0] >= 90:
+            return {'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':float("{0:.2f}".format(rsjarowords[0])), 'sound':float("{0:.2f}".format(rsjarowords[1]))}
+        elif rsjarowords[1] >= 90 and rsjarowords[0] >= 75:
+            return {'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':float("{0:.2f}".format(rsjarowords[0])), 'sound':float("{0:.2f}".format(rsjarowords[1]))}
+    except:
+        return {}
 
-def comparationjaro(name,compare):
+def comparationbyword(name,compare):
     soundex = Soundex()
-    lista1 = name.replace(',','').split(' ')
-    lista2 = compare.replace(',','').split(' ') 
+    lista1 = list(dict.fromkeys(name.replace(',','').split(' ')))
+    lista2 = list(dict.fromkeys(compare.replace(',','').split(' ')))
     cuentaj = 0
     cuentas = 0
     indexj = 0
@@ -92,18 +106,14 @@ def comparationjaro(name,compare):
         for b in lista2: 
             datojaro = jaro.jaro_winkler_metric(a,b)*100
             datosound = jaro.jaro_winkler_metric(soundex.phonetics(a),soundex.phonetics(b))*100
-            
-            if datojaro >= 85:
+            if datojaro >= 90:
                 indexj +=1
                 cuentaj += datojaro
-
-            if datosound >= 85:
+            if datosound >= 90:
                 indexs +=1
                 cuentas += datosound
-            
     cuentaj = cuentaj / max([len(lista1),indexj])
     cuentas = cuentas / max([len(lista1),indexs])
-
     return [cuentaj,cuentas]
 
 
