938d0d95429f3b2834577006845d447d0af3b73e
Joshua Cleveland <joelcld05@gmail.com>
2020-08-30 16:22:42 -0500

    mejoras visuales

--- app/views.py
+++ app/views.py
@@ -45,10 +45,13 @@ def guarda(request):
 @require_http_methods(["GET"])
 def initpage(request):
     downloadfile()
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+    
     connStr = (r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ="+os.path.join(settings.BASE_DIR, 'db.accdb')+";")
     conn = pypyodbc.connect(connStr)
     cur = conn.cursor()
-    query = "SELECT id, Name_EN FROM clientes"
+    query = "SELECT id, Name_EN FROM clientes1"
     cur.execute(query)
     rs = []
     #print(alt)
@@ -65,8 +68,8 @@ def getresult(request):
     if request.method == "POST":
         rs = []
         nombre = request.POST['nombre']
-        response =  comparewithcsv(nombre.upper())
-        totalrs = len(response)
+        response =  comparewithcsv(nombre.replace(',','').upper())
+        totalrs = len(response) 
         if totalrs > 0:
             rs.append({'datos':response,'total':totalrs,'nombre':nombre})
         return JsonResponse(rs,safe=False)
@@ -77,30 +80,40 @@ def comparewithcsv(name):
     results = []
     sdn=[]
     alias = 'a.k.a. '
-    with open('sdn.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile)
-        for row in spamreader:
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+        for rowdata in data:
             try:
+                row=rowdata['data']
+                akadata= rowdata['aka']
                 splitaka= row[11].find(alias)
                 if splitaka > 0:
                     akdata = row[11].split(';')
                     for i in akdata: 
                         if i.find(alias) > 0:
-                            akaname     = i.split("'")[1].upper()
-                            presaundex  = soundex.phonetics(akaname[1])
-                            rsjaro      = jaro.jaro_winkler_metric(name,akaname[1])*100
-                            rssound     = jaro.jaro_winkler_metric(originalsoundx,presaundex)*100
-                            if rsjaro >= maxvalue and rssound >= maxvalue:
-                                results.append([row[0],row[1],akaname,row[3],rsjaro, rssound])
+                            akaname     = i.strip().replace(',','').split("'")[1].upper()
+
+                            comparename=comparationcompletename(name,akaname,originalsoundx)
+                            if comparename[0] >= maxvalue and comparename[1] >= maxvalue:
+                                results.append({'id':row[0],'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjaro, 'sound':rssound})
                             rsjarowords = comparationbyword(name, akaname)
                             if rsjarowords[1] >= maxvalue and rsjarowords[0] >= maxvalue:
                                 results.append({'id':row[0],'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjarowords[0], 'sound':rsjarowords[1]})
-                namecsv     = row[1].upper()
-                presaundex  = soundex.phonetics(namecsv)
-                rsjaro      = jaro.jaro_winkler_metric(name,namecsv)*100
-                rssound     = jaro.jaro_winkler_metric(originalsoundx,presaundex)*100
                 
-                if rsjaro >= maxvalue and rssound >= maxvalue:
+                if len(akadata) > 0:
+                    for i in akadata: 
+                        akaname     = i[3].strip().replace(',','').upper()
+                        comparename=comparationcompletename(name,akaname,originalsoundx)
+                        if comparename[0] >= maxvalue and comparename[1] >= maxvalue:
+                            results.append({'id':row[0]+'-aka' ,'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjaro, 'sound':rssound})
+                        rsjarowords = comparationbyword(name, akaname)
+                        if rsjarowords[1] >= maxvalue and rsjarowords[0] >= maxvalue:
+                            results.append({'id':row[0]+'-aka' ,'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjarowords[0], 'sound':rsjarowords[1]})
+                
+                namecsv     = row[1].replace(',','').upper()
+
+                comparename=comparationcompletename(name,namecsv,originalsoundx)
+                if comparename[0] >= maxvalue and comparename[1] >= maxvalue:
                     results.append({'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':rsjaro, 'sound':rssound})
                 
                 rsjarowords = comparationbyword(name, namecsv)
@@ -111,9 +124,15 @@ def comparewithcsv(name):
     results = sorted(results, key=lambda x: x['jaro'], reverse=True)
     return results
 
+def comparationcompletename(name,compare,sound):
+    presaundex  = soundex.phonetics(compare)
+    rsjaro      = jaro.jaro_winkler_metric(name,compare)*100
+    rssound     = jaro.jaro_winkler_metric(sound,presaundex)*100
+    return [rsjaro,rssound]
+
 
 def comparationbyword(name,compare):
-    #soundex = Soundex()
+
     lista1 = list(dict.fromkeys(name.replace(',','').split(' ')))
     lista2 = list(dict.fromkeys(compare.replace(',','').split(' ')))
 
@@ -125,7 +144,7 @@ def comparationbyword(name,compare):
 
     for a in lista1: 
         for b in lista2: 
-            datojaro = jaro.jaro_winkler_metric(a,b)*100
+            datojaro = jaro.jaro_winkler_metric(a.strip(),b.strip())*100
             datosound = jaro.jaro_winkler_metric(soundex.phonetics(a),soundex.phonetics(b))*100
             if datojaro >= maxvalue:
                 indexj +=1
@@ -133,40 +152,47 @@ def comparationbyword(name,compare):
             if datosound >= maxvalue:
                 indexs +=1
                 cuentas += datosound
+
+
     cuentaj = cuentaj / max([len(lista1),indexj])
     cuentas = cuentas / max([len(lista1),indexs])
+    
     return [cuentaj,cuentas]
 
 
-def comparationcompletename():
-    presaundex  = soundex.phonetics(akaname[1])
-    rsjaro      = jaro.jaro_winkler_metric(name,akaname[1])*100
-    rssound     = jaro.jaro_winkler_metric(originalsoundx,presaundex)*100
-    return [rsjaro,rssound]
+
 
 
 
 def downloadfile():
     try:
         sdn=[]
-        alt=[]
+        alt={}
+        name=datetime.datetime.now().strftime("%d-%m-%Y")
         urlsdn = 'https://www.treasury.gov/ofac/downloads/sdn.csv'
         urlalt = 'https://www.treasury.gov/ofac/downloads/alt.csv'
-        urllib.request.urlretrieve(urlsdn, os.path.join(settings.BASE_DIR, 'sdn.csv'))
-        urllib.request.urlretrieve(urlalt, os.path.join(settings.BASE_DIR, 'alt.csv'))
-        with open('sdn.csv', newline='') as csvfile:
+        urllib.request.urlretrieve(urlsdn, os.path.join(settings.BASE_DIR, 'static/data/sdn'+name+'.csv'))
+        urllib.request.urlretrieve(urlalt, os.path.join(settings.BASE_DIR, 'static/data/alt'+name+'.csv'))
+
+        with open('static/data/alt.csv', newline='') as csvfile:
             spamreader = csv.reader(csvfile)
             for row in spamreader:
-                sdn.append(row)
+                if not row[0] in alt:
+                    alt[row[0]]=[row]
+                else:
+                    alt[row[0]].append(row)
 
-
-        cache.set('sdn', sdn)
-
-        with open('alt.csv', newline='') as csvfile:
+        with open('static/data/sdn.csv', newline='') as csvfile:
             spamreader = csv.reader(csvfile)
             for row in spamreader:
-                alt.append(row)
-
+                akadato=[]
+                if row[0] in alt:
+                    akadato=alt[row[0]]
+                item = {'data':row,'aka':akadato}
+                sdn.append(item)
+        
+        with open('data.json', 'w') as write_file:
+            json.dump(sdn,write_file)
     except:
         pass
             
