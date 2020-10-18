73122b4256c229aeaa45a34e3fc3807298bd529f
Joshua Cleveland <joelcld05@gmail.com>
2020-10-18 14:02:23 -0500

    mejoras de algoritmo

--- app/views.py
+++ app/views.py
@@ -7,18 +7,23 @@ from algoritmos import settings
 
 import urllib.request
 import pandas as pd
-import datetime
+import numpy as np
 import functools
 import pypyodbc
+import datetime
+import sqlite3
 import jaro
 import json
 import csv
 import os
 
 
+
+comparationscon = sqlite3.connect('comparations.db',check_same_thread=False)
+
 soundex = Soundex()
 maxvalue = 85
-connStr = (r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ="+os.path.join(settings.BASE_DIR, 'db.accdb')+";")
+connStr = (r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ="+os.path.join(settings.BASE_DIR, 'GFUX_DWH2.accdb')+";")
 
 @require_http_methods(["GET"])
 def index(request):
@@ -46,13 +51,13 @@ def login(request):
             rs=row
         if rs[0]>=1:
             username = request.POST['user']
-            response = HttpResponseRedirect('resultdos')
+            response = HttpResponseRedirect('reportes')
             response.set_cookie('username', request.POST['user'])
         else:
             response = HttpResponse(render(request, 'index.html',{"message" : 'Credenciales incorrectas'}))
         return response
     else:
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect('reportes')
 
 
 def logout(request):
@@ -63,7 +68,7 @@ def logout(request):
 
 
 
-@csrf_exempt 
+@csrf_exempt
 def guarda(request):
     if request.method == "POST":
         try:
@@ -75,9 +80,10 @@ def guarda(request):
             conn = pypyodbc.connect(connStr)
             cur = conn.cursor()
             for a in lista1: 
-                query = "insert into DatosGuardados(idclinete, observacion,idcompara,nombrecompara,reporta,jaro,sound,report)"
-                query +=  " values ("+idcliente+",'"+request.POST['observacion-'+a].replace("'",'')+"','"+a+"','"+request.POST['nombrecompara-'+a].replace("'",'')
-                query +=  "','"+request.COOKIES['username'].replace("'",'')+"',"+request.POST['jaro-'+a]+","+request.POST['sound-'+a]+","+idinsert+" )"
+                accion=request.POST["conservar-"+a]
+                query = "insert into tbl_ofac_reportados(idclinete,observacion,idcompara,nombrecompara,reporta,jaro,sound,report,accion)"
+                query +=  " values ('"+idcliente+"','"+request.POST['observacion-'+a].replace("'",'')+"','"+a+"','"+request.POST['nombrecompara-'+a].replace("'",'')
+                query +=  "','"+request.COOKIES['username'].replace("'",'')+"',"+request.POST['jaro-'+a]+","+request.POST['sound-'+a]+","+idinsert+",'"+accion+"' )"
 
                 cur.execute(query)
             cur.commit()
@@ -98,7 +104,7 @@ def initpage(request):
         verificareporte(request.COOKIES['username'],idinsert)
         conn = pypyodbc.connect(connStr)
         cur = conn.cursor()
-        query = 'SELECT id, Nombre, Apellidos FROM clientes2'
+        query = 'SELECT id, name, lastname FROM OFAC'
         cur.execute(query)
         rs = []
         while True:
@@ -117,13 +123,13 @@ def initpage(request):
 def verificareporte(username,idinsert):
     conn = pypyodbc.connect(connStr)
     cur = conn.cursor()
-    query = 'SELECT id FROM reports where id='+idinsert
+    query = 'SELECT id FROM tbl_ofac_reportes where id='+idinsert
     cur.execute(query)
     row = cur.fetchall()
     if len(row)==0:
         conn2 = pypyodbc.connect(connStr)
         cur2 = conn2.cursor()
-        query = "insert into reports(ID,usuario) values ("+idinsert+",'"+ username+"');"
+        query = "insert into tbl_ofac_reportes(id,usuario) values ("+idinsert+",'"+ username+"');"
         cur2.execute(query)
         cur2.commit()
         cur2.close()
@@ -138,93 +144,114 @@ def getresult(request):
         rs = []
         nombre = request.POST['nombre']
         idcompara = request.POST['id']
+
+        cursor = comparationscon.execute("SELECT idofac from comparations where idcliente='"+idcompara+"'")
+        rows = cursor.fetchall()
+        skip = []
+        for row in rows:
+            skip.append(row[0])
+
         conn = pypyodbc.connect(connStr)
         cur = conn.cursor()
-        query = 'SELECT idcompara FROM DatosGuardados where idclinete ='+idcompara
+        query = "SELECT idcompara,idclinete FROM tbl_ofac_reportados where idclinete ='"+idcompara+"' and accion='Reportado'"
         cur.execute(query)
-        skip = []
+
         while True:
             row = cur.fetchone()
             if row is None:
                 break
             lista = list(dict.fromkeys(row[0].split('-')))
-            skip.append(lista[0])
+            idofac=lista[0].split("-")
+            skip.append(int(idofac[0]))
+
         cur.commit()
         cur.close()
         conn.close()
-        response =  comparewithcsv(nombre.replace(',','').upper(), skip)
+    
+        response =  comparewithcsv(nombre.replace(',','').upper(), skip,idcompara)
         totalrs = len(response) 
         if totalrs > 0:
             rs.append({'datos':response,'total':totalrs,'nombre':nombre})
         return JsonResponse(rs,safe=False)
 
 
-def comparewithcsv(name,skip):
+def comparewithcsv(name,skip,idcompara):
     originalsoundx = soundex.phonetics(name)
     results = []
     sdn=[]
     alias = 'a.k.a. '
-
     with open('data.json') as json_file:
         data = json.load(json_file)
         for rowdata in data:
+            guardaofac=0
             try:
                 row=rowdata['data']
-                if row[0] in skip:
-                    continue
-                akadata= rowdata['aka']
-                splitaka= row[11].find(alias)
-                if splitaka > 0:
-                    akdata = row[11].split(';')
-                    for i in akdata: 
-                        if i.find(alias) > 0:
-                            akaname     = i.strip().replace(',','').split("'")[1].upper()
+                if int(row[0]) not in skip:
+                    akadata= rowdata['aka']
+                    splitaka= row[11].find(alias)
+                    if splitaka > 0:
+                        akdata = row[11].split(';')
+                        for i in akdata: 
+                            if i.find(alias) > 0:
+                                akaname     = i.strip().replace(',','').split("'")[1].upper()
+                                comparename=comparationcompletename(name,akaname,originalsoundx)
+                                if comparename[0] >= maxvalue and comparename[1] >= maxvalue:
+                                    results.append({'id':row[0],'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjaro, 'sound':rssound})
+                                    guardaofac=1
+                                    continue
+
+                                rsjarowords = comparationbyword(name, akaname)
+                                if rsjarowords[1] >= maxvalue and rsjarowords[0] >= maxvalue:
+                                    results.append({'id':row[0],'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjarowords[0], 'sound':rsjarowords[1]})
+                                    guardaofac=1
+
+                    if len(akadata) > 0:
+                        for i in akadata: 
+                            akaname     = i[3].strip().replace(',','').upper()
                             comparename=comparationcompletename(name,akaname,originalsoundx)
                             if comparename[0] >= maxvalue and comparename[1] >= maxvalue:
-                                results.append({'id':row[0],'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjaro, 'sound':rssound})
+                                results.append({'id':row[0]+'-'+i[1] ,'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjaro, 'sound':rssound})
+                                guardaofac=1
                                 continue
 
                             rsjarowords = comparationbyword(name, akaname)
                             if rsjarowords[1] >= maxvalue and rsjarowords[0] >= maxvalue:
-                                results.append({'id':row[0],'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjarowords[0], 'sound':rsjarowords[1]})
+                                results.append({'id':row[0]+'-'+i[1] ,'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjarowords[0], 'sound':rsjarowords[1]})
+                                guardaofac=1
+                    
+                    namecsv     = row[1].replace(',','').upper()
+                    comparename=comparationcompletename(name,namecsv,originalsoundx)
+                    if comparename[0] >= maxvalue and comparename[1] >= maxvalue:
+                        results.append({'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':rsjaro, 'sound':rssound})
+                        guardaofac=1
+                        continue
 
-                if len(akadata) > 0:
-                    for i in akadata: 
-                        akaname     = i[3].strip().replace(',','').upper()
-                        comparename=comparationcompletename(name,akaname,originalsoundx)
-                        if comparename[0] >= maxvalue and comparename[1] >= maxvalue:
-                            results.append({'id':row[0]+'-'+i[1] ,'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjaro, 'sound':rssound})
-                            continue
+                    rsjarowords = comparationbyword(name, namecsv)
+                    if rsjarowords[1] >= maxvalue and rsjarowords[0] >= maxvalue:
+                        results.append({'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':rsjarowords[0], 'sound':rsjarowords[1]})
+                        guardaofac=1
 
-                        rsjarowords = comparationbyword(name, akaname)
-                        if rsjarowords[1] >= maxvalue and rsjarowords[0] >= maxvalue:
-                            results.append({'id':row[0]+'-'+i[1] ,'nombre':row[1],'alias':akaname,'tipo':row[3],'jaro':rsjarowords[0], 'sound':rsjarowords[1]})
-                
-                namecsv     = row[1].replace(',','').upper()
-                comparename=comparationcompletename(name,namecsv,originalsoundx)
-                if comparename[0] >= maxvalue and comparename[1] >= maxvalue:
-                    results.append({'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':rsjaro, 'sound':rssound})
-                    continue
-
-                rsjarowords = comparationbyword(name, namecsv)
-                if rsjarowords[1] >= maxvalue and rsjarowords[0] >= maxvalue:
-                    results.append({'id':row[0],'nombre':row[1],'alias':'','tipo':row[3],'jaro':rsjarowords[0], 'sound':rsjarowords[1]})  
+                    if guardaofac==0:
+                        cur = comparationscon.cursor()
+                        cur.execute("INSERT INTO comparations (idcliente, idofac)  VALUES ('"+idcompara+"', "+row[0]+");")
+                    
+                    
             except:
-                pass
+                    pass
     results = sorted(results, key=lambda x: x['jaro'], reverse=True)
+    comparationscon.commit()
     return results
 
 def comparationcompletename(name,compare,sound):
     presaundex  = soundex.phonetics(compare)
-    rsjaro      = jaro.jaro_metric(name,compare)*100
-    rssound     = jaro.jaro_metric(sound,presaundex)*100
+    rsjaro      = jaro.jaro_winkler_metric(name,compare)*100
+    rssound     = jaro.jaro_winkler_metric(sound,presaundex)*100
     return [rsjaro,rssound]
 
 
 def comparationbyword(name,compare):
-
-    lista1 = list(dict.fromkeys(name.replace(',','').split(' ')))
-    lista2 = list(dict.fromkeys(compare.replace(',','').split(' ')))
+    lista1 = list(dict.fromkeys(name.replace(',','').replace("'",'').split(' ')))
+    lista2 = list(dict.fromkeys(compare.replace(',','').replace("'",'').split(' ')))
 
     arrayj= []
     cantidadenlista=0
@@ -235,8 +262,13 @@ def comparationbyword(name,compare):
 
     for a in lista1: 
         for b in lista2: 
-            datojaro = jaro.jaro_metric(a.strip(),b.strip())*100
-            datosound = jaro.jaro_metric(soundex.phonetics(a),soundex.phonetics(b))*100
+
+            try:
+                datojaro = jaro.jaro_winkler_metric(a.strip(),b.strip())*100
+                datosound = jaro.jaro_winkler_metric(soundex.phonetics(a),soundex.phonetics(b))*100
+            except:
+                pass
+
             if datojaro >= maxvalue:
                 indexj +=1
                 cuentaj += datojaro
@@ -289,10 +321,10 @@ def downloadfile():
 def reportpage(request):
     if 'username' in request.COOKIES:
         idinsert = datetime.datetime.now().strftime("%d%m%Y")
-        connStr = (r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ="+os.path.join(settings.BASE_DIR, 'db.accdb')+";")
+        #connStr = (r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ="+os.path.join(settings.BASE_DIR, 'db.accdb')+";")
         conn = pypyodbc.connect(connStr)
         cur = conn.cursor()
-        query = 'select str(reports.fecha) as fecha, reports.usuario, count(DatosGuardados.report) as totales,reports.ID from reports inner join DatosGuardados on DatosGuardados.report=reports.ID group by reports.ID, reports.fecha, reports.usuario'
+        query = 'select top 12 str(tbl_ofac_reportes.fecha) as fecha, tbl_ofac_reportes.usuario, count(tbl_ofac_reportes.ID) as totales,tbl_ofac_reportes.ID from tbl_ofac_reportes inner join tbl_ofac_reportados on tbl_ofac_reportados.report=tbl_ofac_reportes.ID group by tbl_ofac_reportes.ID, tbl_ofac_reportes.fecha, tbl_ofac_reportes.usuario order by tbl_ofac_reportes.fecha desc'
         cur.execute(query)
         rs = []
         while True:
@@ -310,17 +342,17 @@ def reportpage(request):
 def printReport(request,idreport):
     if 'username' in request.COOKIES:
         idinsert = datetime.datetime.now().strftime("%d%m%Y")
-        connStr = (r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ="+os.path.join(settings.BASE_DIR, 'db.accdb')+";")
+        #connStr = (r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ="+os.path.join(settings.BASE_DIR, 'db.accdb')+";")
         conn = pypyodbc.connect(connStr)
         cur = conn.cursor()
-        query = 'select reporta,fecha,idclinete, nombre, apellidos, cedula,observacion,idcompara,nombrecompara,jaro,sound from DatosGuardados inner join clientes on clientes.id=DatosGuardados.idclinete where report='+str(idreport)
+        query = 'select reporta,fecha,idclinete, name, lastname, id,observacion,idcompara,nombrecompara,jaro,sound, accion from tbl_ofac_reportados inner join OFAC on OFAC.id=tbl_ofac_reportados.idclinete where report='+str(idreport)
         cur.execute(query)
         row = cur.fetchall()
 
         file_name = str(idreport)+'-reporte.xlsx'
         path_to_file = settings.BASE_DIR+'/static/exports/'
 
-        df = pd.DataFrame(row, columns = ['Reportado Por','Fecha reportado','ID Cliente', 'Nombres', 'Apellidos', 'Cédula','Observacion','ID OFAC','Nombre OFAC','Jaro','Sound'])
+        df = pd.DataFrame(row, columns = ['Reportado Por','Fecha reportado','ID Cliente', 'Nombres', 'Apellidos', 'Cédula','Observación','ID OFAC','Nombre OFAC','Jaro','Sound','Acción'])
         df.to_excel(path_to_file+file_name, index = False, header=True)
         
         file_path=path_to_file+file_name
